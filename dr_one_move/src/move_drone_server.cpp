a6693cd7fb120029a83185a2c8a54a5d36d9894e
Abhishek Choudhary <abhishekchou@gmail.com>
2017-05-09 19:23:29 +0200

    Server bug fixing to communicate with the frontier node

--- dr_one_move/src/move_drone_server.cpp
+++ dr_one_move/src/move_drone_server.cpp
@@ -1,16 +1,19 @@
 #include <ros/ros.h>
 #include <cmath>
+
 #include <actionlib/server/simple_action_server.h>
 #include <dr_one_move/move_droneAction.h>
+
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Pose2D.h>
-#include <move_base/move_base.h>
+// #include <dr_one_move/move_base.h>
 #include <tf/transform_datatypes.h>
 
 using namespace ros;
 using namespace std;
 using namespace actionlib;
 using namespace tf;
+using namespace dr_one_move;
 
 
 class move_drone
@@ -23,66 +26,47 @@ protected:
     dr_one_move::move_droneFeedback feedback_;
     dr_one_move::move_droneResult result_;
 
+private:
+    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+    void exCallback(const dr_one_move::move_droneGoalConstPtr &target);
+    double getYAW_(geometry_msgs::Quaternion quat);
+    double distance(geometry_msgs::Pose2D start, geometry_msgs::Pose2D end);
+
 public:
-
-    move_drone(string name);
-    ~move_drone();
-
+    // move_drone(string name);
+    // ~move_drone();
     Subscriber pose_sub;
     Publisher goal_pub;
     string _pose_topic, _goal_topic;
 
-    geometry_msgs::Pose2D current_pose; //Save pose from mavros here
+    geometry_msgs::Pose2D pose_2d; //Save pose from mavros here
     geometry_msgs::Pose2D goal;
-    vector<geometry_msgs::Pose2D> path;
+    geometry_msgs::PoseStamped pose;
+    // vector<geometry_msgs::Pose2D> path;
 
-private:
-    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
-    void exCallback(const dr_one_move::move_droneGoalConstPtr &goal);
-    double getYAW(geometry_msgs::Quaternion quat);
-    double distance(geometry_msgs::Pose2D start, geometry_msgs::Pose2D end);
-
-};
-
-move_drone::move_drone(string name) :
-    action(nh_, name, boost::bind(&move_droneAction::executeCB, this, _1), false),
+move_drone(string name) :
+    action(nh_, name, boost::bind(&move_drone::exCallback, this, _1), false),
     mover_drone(name)
 {
     action.start();
     ROS_WARN("_move_drone_:Constructing an object of class MOVE_DRONE");
-    pose_sub = nh_.subscribe<geometry_msgs::PoseStamped>(_pose_topic.data(), 5, &move_drone::poseCallback, this);
+    nh_.getParam("pose_topic",_pose_topic);
+    nh_.getParam("goal_topic",_goal_topic);
+    pose_sub = nh_.subscribe<nav_msgs::Odometry>(_pose_topic.data(), 5, &move_drone::poseCallback, this);
     goal_pub = nh_.advertise<geometry_msgs::Pose2D>(_goal_topic.data(),5);
 }
 
-move_drone::~move_drone()
+~move_drone()
 {
     ROS_WARN("_move_drone_:Deleting an object of class MOVE_DRONE");
 }
+};
 
-double move_drone::getYAW(geometry_msgs::Quaternion quat)
+
+double move_drone::getYAW_(geometry_msgs::Quaternion quat)
 {
-    double roll, pitch, yaw;
-    Matrix3x3 m(quat);
-    m.getRPY(roll, pitch, yaw);
-
-    return yaw;
-}
-
-double move_drone::distance(geometry_msgs::Pose2D start, geometry_msgs::Pose2D end)
-{
-    double distance;
-    double x_diff = start.x - end.x;
-    double y_diff = start.y - end.y;
-    distance = sqrt( pow(x_diff,2) + pow(y_diff,2) );
-    return distance;
-}
-
-double move_drone::getYAW(geometry_msgs::Quaternion quat)
-{
-    double roll, pitch, yaw;
-    Matrix3x3 m(quat);
-    m.getRPY(roll, pitch, yaw);
-
+    double yaw;
+    yaw = getYaw(quat);
     return yaw;
 }
 
@@ -98,21 +82,21 @@ double move_drone::distance(geometry_msgs::Pose2D start, geometry_msgs::Pose2D e
 //Save pose from EKF_POSE/MAVROS_POSE in a PoseStamped variable
 void move_drone::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-    geometry_msgs::PoseStamped pose;
     pose.header = msg->header;
     pose.pose = msg->pose;
 
     //Save from PoseStamped to Pose2D
-    current_pose.x = pose.pose.position.x;
-    current_pose.y = pose.pose.position.y;
-    current_pose.theta = getYAW(pose.pose.orientation);
+    pose_2d.x = pose.pose.position.x;
+    pose_2d.y = pose.pose.position.y;
+    pose_2d.theta = getYAW_(pose.pose.orientation);
 }
 
 
-void move_drone::executeCB(const dr_one_move::move_droneGoalConstPtr &target)
+void move_drone::exCallback(const dr_one_move::move_droneGoalConstPtr &target)
 {
     Rate loop_rate(20);
-    move_base_msgs::MoveBaseGoal new_target;
+    // move_base_msgs::MoveBaseGoal new_target;
+    move_droneGoal new_target;
     bool success = false;
     ROS_WARN("_move_drone_:Sending Setpoint to /mavros/setpoints for drone to navigate");
 
@@ -124,11 +108,11 @@ void move_drone::executeCB(const dr_one_move::move_droneGoalConstPtr &target)
             if(action.isNewGoalAvailable())
             {
                 ROS_WARN("_move_drone_:New Goal Received from Exploration");
-                new_target = action.acceptNewGoal();
+                new_target = *action.acceptNewGoal();
 
                 goal.x = new_target.target_pose.pose.position.x;
                 goal.y = new_target.target_pose.pose.position.y;
-                goal.theta = getYAW(new_target.target_pose.pose.orientation);
+                goal.theta = getYAW_(new_target.target_pose.pose.orientation);
             }
             else
                 action.setPreempted();
@@ -136,15 +120,15 @@ void move_drone::executeCB(const dr_one_move::move_droneGoalConstPtr &target)
 
         goal.x = target->target_pose.pose.position.x;
         goal.y = target->target_pose.pose.position.y;
-        goal.theta = getYAW(target->target_pose.pose.orientation);
+        goal.theta = getYAW_(target->target_pose.pose.orientation);
 
         goal_pub.publish(goal);
         spinOnce;
-        loop_rate.sleep();
 
-        if(distance(current_pose, goal) < 0.20)//If within 20cm of goal
+        loop_rate.sleep();
+        if(distance(pose_2d, goal) < 0.20)//If within 20cm of goal
         {
-            feedback_.current_pose = current_pose;
+            feedback_.current_pose = pose;
             action.publishFeedback(feedback_);
             success = true;
         }
@@ -155,7 +139,7 @@ void move_drone::executeCB(const dr_one_move::move_droneGoalConstPtr &target)
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "move_drone");
-  move_droneAction move_drone("move_drone");
+  move_drone move_drone("move_drone");
   ros::spin();
 
   return 0;
